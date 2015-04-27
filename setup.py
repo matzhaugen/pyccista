7bb1dc1bbdf8a2c1cdda0063592a7235649cfc5b
Sang Oh <syoh@lbl.gov>
2015-04-27 15:08:21 -0700

    MANIFEST.in determines which files go into sdist package

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except AttributeError:
 # concord extension module
 _ccista = Extension('_ccista',
                     ['concord/ccista.i','concord/ccista.cpp'],
-                    include_dirs = [numpy_include, 'concord/'],
+                    include_dirs = [numpy_include],
                     swig_opts = ['-c++'])
 
 setup(name        = 'concord',

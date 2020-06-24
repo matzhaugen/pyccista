2ff89300c7d332e6072bb1e0c73c95f6652a1b92
Matz Haugen <matz.haugen@cognite.com>
2020-06-24 12:38:19 +0200

    add scripts

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -1,5 +1,6 @@
-yum install -y pcre-devel
-curl -L https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz -o tarfiles/eigen-3.3.7 &&\
+
+mkdir tarfiles && yum install -y pcre-devel
+curl -L https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz -o tarfiles/eigen-3.3.7.tar.gz &&\
 tar -xzvf tarfiles/eigen-3.3.7.tar.gz
 
 curl -L https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download?use_mirror=iweb -o tarfiles/swig-3.0.12.tar.gz &&\

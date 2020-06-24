5d28ec8cdb2718f5baeea8f217623be5933d85eb
Matz Haugen <matz.haugen@cognite.com>
2020-06-24 14:09:14 +0200

    debug

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -5,7 +5,7 @@ tar -xzvf tarfiles/eigen-3.3.7.tar.gz > /dev/null 2>&1
 
 curl -L https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download?use_mirror=iweb -o tarfiles/swig-3.0.12.tar.gz  &&\
 tar -xzvf tarfiles/swig-3.0.12.tar.gz > /dev/null 2>&1 &&\
-cd swig-3.0.12 && ./configure && make -j4 && make install -j4 && cd ..
+cd swig-3.0.12 && ./configure > /dev/null 2>&1 && make -j4 > /dev/null 2>&1 && make install -j4 > /dev/null 2>&1 && cd ..
 
 # tar -xzvf tarfiles/libffi-3.3.tar.gz &&\
 # cd libffi-3.3 && ./configure && make -j4 && make install -j4 && cd ..

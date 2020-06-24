b4497ccb1fb30a04b1b3f4ed2300a2be9992220a
Matz Haugen <matz.haugen@cognite.com>
2020-06-24 12:53:11 +0200

    test cache

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -1,10 +1,10 @@
 
 mkdir tarfiles && yum install -y pcre-devel
 curl -L https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz -o tarfiles/eigen-3.3.7.tar.gz &&\
-tar -xzvf tarfiles/eigen-3.3.7.tar.gz
+tar -xzvf tarfiles/eigen-3.3.7.tar.gz > /dev/null 2>&1
 
-curl -L https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download?use_mirror=iweb -o tarfiles/swig-3.0.12.tar.gz &&\
-tar -xzvf tarfiles/swig-3.0.12.tar.gz &&\
+curl -L https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download?use_mirror=iweb -o tarfiles/swig-3.0.12.tar.gz  &&\
+tar -xzvf tarfiles/swig-3.0.12.tar.gz > /dev/null 2>&1 &&\
 cd swig-3.0.12 && ./configure && make -j4 && make install -j4 && cd ..
 
 # tar -xzvf tarfiles/libffi-3.3.tar.gz &&\

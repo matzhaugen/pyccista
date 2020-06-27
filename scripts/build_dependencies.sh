57c7b85405d15da563167592ff80f839feaf83a8
Matz Haugen <matz.haugen@cognite.com>
2020-06-27 08:40:00 +0200

    move docker

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -4,8 +4,11 @@ system=$(uname -a)
 if [[ ${system} == *"Darwin"* ]]; then
 	./scripts/install_libraries.sh
 else
-	
+	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 	if [[ "$(docker images -q $1:$2 2> /dev/null)" == "" ]]; then
-	  docker build -t $1:$2 -f Dockerfile_builder .
+		docker build -t $1:$2 -f Dockerfile_builder .
+		docker push -$1:$2
+	else
+		docker pull matzhaugen/concord-builder:$TAG
 	fi
 fi

3b705b6b7e02d71030d4a8d8f28a11d5f6b41286
Matz Haugen <mhaugen@Matz-M1-Max.local>
2024-10-20 14:56:15 +0200

    add robust selection

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,29 +1,36 @@
 from __future__ import annotations
-
+import time
 import pyconcord as pc
 
-
-# import pyconcord as pc
 import numpy as np
 
 
 n_values = [0, 1, 2, 17]
+
+def test_robust_concord():
+    x = np.random.randn(13, 9)
+
+    n, p = x.shape
+    s = time.time()
+    lam = pc.RobustSelection(x, alpha=0.95, B=200, with_diag=False)
+    e = time.time()
+    print(f"Time to finish robust selection: {e - s:.2f}")
+    s = time.time()
+    omega = pc.concord(x, lambda1=lam)
+    e = time.time()
+    print(f"Time to finish concord: {e - s:.2f}")
+    
+    cov = np.round(omega.todense(), 2)
+    assert ~np.isinf(cov[0,0])
+    assert cov[0, 1] == cov[1, 0]
 
 def test_basic_concord():
     x = np.random.randn(13, 9)
 
     n, p = x.shape
     
-    # TODO: allow passing initial guess
-    # x0 = identity(p).tocoo()
-    # i, j, data = pc.concord(
-    #     x, x0.row, x0.col, x0.data, penalty1=0.3, penalty2=0,
-    # )
-    # i, j, data = pc.concord(x, lambda1=0.3)
-    # omega = csr_matrix((data, vstack((i, j))), shape=(p, p))
     print(pc.concord.__doc__)
     omega = pc.concord(x, lambda1=0.3)
-    # omega = pc.concord(Y=x, lambda1=0.3)
     
     cov = np.round(omega.todense(), 2)
     assert ~np.isinf(cov[0,0])
@@ -36,14 +43,7 @@ def test_concord_w_guess():
 
     n, p = x.shape
     
-    # TODO: allow passing initial guess
     x0 = identity(p).tocoo()
-    # i, j, data = pc.concord(
-    #     x, x0.row, x0.col, x0.data, penalty1=0.3, penalty2=0,
-    # )
-    # i, j, data = pc.concord(x, lambda1=0.3)
-    # omega = csr_matrix((data, vstack((i, j))), shape=(p, p))
-    print(pc.concord.__doc__)
     
     omega = pc.concord(x, x0=x0, lambda1=0.3)
     cov = np.round(omega.todense(), 2)
